6ada7f4fa7b1bf819eb34f0f0bbf9f2b53ab30a3
五十嵐翔 <igara1119@gmail.com>
2024-05-18 16:01:38 +0900

    vrt

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { withScreenshot } from "storycap";
 
 const preview: Preview = {
   parameters: {
@@ -8,7 +9,24 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    layout: 'fullscreen',
+    screenshot: {
+      viewports: {
+        large: {
+          width: 1024,
+          height: 768,
+        },
+        small: {
+          width: 320,
+          height: 568,
+        },
+      },
+    },
   },
+  decorators: [
+    // @ts-ignore
+    withScreenshot,
+  ]
 };
 
 export default preview;
